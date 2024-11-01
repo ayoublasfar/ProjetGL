c3384535d6fdb2b36b92fb7fd458e2722745c834
HP <HP@10.10.1.188>
2024-11-01 14:26:22 +0100

    Added C2

---
+++ ma.ac.ensmr/src/ma/ac/ensmr/Class.java
@@ -0,0 +1,14 @@
+package ma.ac.ensmr;
+
+public class Class {
+
+	public Class() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a Java class!");
+	}
+
+}
