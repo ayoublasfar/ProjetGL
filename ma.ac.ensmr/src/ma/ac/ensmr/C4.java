6dcc54ce141fb1da961c48add0f4657d702ec833
HP <HP@DESKTOP-LVVRQPE>
2024-11-03 10:54:55 +0100

    Added C4

---
+++ ma.ac.ensmr/src/ma/ac/ensmr/C4.java
@@ -0,0 +1,14 @@
+package ma.ac.ensmr;
+
+public class C4 {
+
+	public C4() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
